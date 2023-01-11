6ccb60ccf098ee1fc8be0c9ea14e7abf126e1554
ekasaputra0324 <paijomr4221@gmail.com>
2023-01-11 17:49:59 +0000

    create role users and admin

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {Client} = require('pg');
-const bcrypt = require('bcrypt');
 const session = require('express-session');
 const port = 7000;
 const path = require('path');
-const flash = require('flash');
-const { conn } = require('./connection/conn')
+const  {flash} = require('express-flash-message');
 const app = express();
-const router = express.Router();
 const indexRoute = require('./router/user')
+
+
 app.use(session({
     secret: 'secret',
     resave: false,
     saveUninitialized: false
 }));
-app.use(flash()); 
 app.set('views', path.join(__dirname, './view'))
 app.set('view engine' , 'ejs');
 app.use(express.json())
@@ -24,6 +21,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.use('/' , indexRoute);
+
 
 app.listen(port,(err) => {
     if (err) {
--- router/user.js
+++ router/user.js
@@ -1,16 +1,17 @@
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const route = express.Router();
+// const session = require('express-session')
 const client = require('../connection/conn');
-var bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
+const flash = require('connect-flash');
 
 
-
-
-router.get('/resgiter', (req,res) => {
-    res.render('register', {title: 'SELES-APP | Login'})
+route.get('/resgiter', (req,res) => { 
+    const register = '';
+    res.render('register', {title: 'SELES-APP | Register', message: register})
 });
-router.post('/resgiter' , (req, res) =>{
+route.post('/resgiter' , (req, res) =>{
     const {name , email, password} = req.body;
     console.log(
         name,
@@ -21,13 +22,15 @@ router.post('/resgiter' , (req, res) =>{
     client.query(`SELECT * FROM users WHERE email = '${email}'`, (err, result) => {
         if (err) { throw err }
         if (result.rowCount > 0) {
-           res.redirect('/resgiter')
+            const register = 'failed';
+            res.render('register', {title: 'SELES-APP | Register', message: register})
         }else{
             let hash = bcrypt.hashSync(password, 10);
             console.log(hash);
             client.query(`INSERT INTO users(name , email, password) VALUES ('${name}','${email}','${hash}') ` , (err, result) => {
             if (!err) {
-                res.redirect('/')
+                const register = 'success'
+                res.render("register", {title: 'SELES-APP | Register', message: register})
             }else{
                 res.redirect('/resgiter')
             }
@@ -39,39 +42,57 @@ router.post('/resgiter' , (req, res) =>{
 });
 
 // login
-router.get('/', (req, res) => {
+route.get('/', (req, res) => {
     if (req.session.email != null) {
-        res.redirect('/home')
+        res.redirect('/home/users')
     }
-    const message = 'sasa';
-    res.render("login", {title: 'SELES-APP | Login', message: message });
+    const login = []
+    res.render("login", {title: 'SELES-APP | Login', message: login });
 });
-router.post('/auth' , (req, res) => {
+route.post('/auth' , (req, res) => {
     const email = req.body.email;
-    
     client.query(`select * from users WHERE email = '${email}'`, (err, result) =>{
         if(err) throw err;
             if( result.rows.length && bcrypt.compareSync(req.body.password, result.rows[0].password)){
                 req.session.email = email;
-                res.redirect('/home');
+                res.redirect('/home/users');
               }else{
-                res.redirect('/')    
+                const login = 'failed'
+                res.render("login", {title: 'SELES-APP | Login', message: login });    
               } 
         });  
 });
 
-router.get('/home' , (req, res) =>{
+route.get('/home/users' , (req, res) =>{
     if (req.session.email == null ) {
         res.redirect('/')
     }
-    res.render("home", {message: req.session.email})
+    client.query(`select * from users WHERE email = '${req.session.email}'` , (err, result) => {
+        if (!err) {
+            const users = result.rows[0];
+            console.log(users);
+            res.render("home", {message: req.session.email, user: users});
+        }else{
+            console.log(err);
+        }
+    });
+});
+route.get('/home/admin' , (req, res) => {
+    client.query(`select role.name from  users JOIN role ON users.role_id = role.id WHERE users.email = '${req.session.email}'`, (err, result) => {
+        if (!err) {
+            const role = result.rows[0].name;
+            if (role == 'user') {
+                res.redirect('/home/users')
+            }
+        }
+    });
 });
 
-router.get('/logout', function(req, res, next){
+route.get('/logout', function(req, res, next){
     if(req.session.email){
       req.session.destroy();
       res.redirect('/');
     }
 })
 
-module.exports = router;
+module.exports = route;
