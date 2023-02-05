e54838b141f4bee5ad8ac3344e8f99c6aacacc23
ekasaputra0324 <paijomr4221@gmail.com>
2023-02-05 17:11:46 +0000

    changes logo

--- connection/conn.js
+++ connection/conn.js
@@ -2,7 +2,7 @@ const {Client} = require('pg');
 
 const client  = new Client({
     user: 'postgres',
-    password: 'root',
+    password: 'admin',
     host: 'localhost',
     port:  5432,
     database: 'seles-app'
