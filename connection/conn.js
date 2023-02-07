2eca2c5ac1bef822486193dd1951b7744c7313c3
anifa <anifaaulia1974@gmail.com>
2023-02-07 09:24:56 +0700

    add category

--- connection/conn.js
+++ connection/conn.js
@@ -2,7 +2,7 @@ const {Client} = require('pg');
 
 const client  = new Client({
     user: 'postgres',
-    password: 'admin',
+    password: 'root',
     host: 'localhost',
     port:  5432,
     database: 'seles-app'
